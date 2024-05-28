6428fd21f2166f63e2d04dc36f1be77dc89cb476
kaotikus27 <73923255+kaotikus27@users.noreply.github.com>
2024-05-28 11:37:37 +0800

    initial commmit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
+  <router-view/>
+</template>
+
+<style>
+
+
+#app {
+  font-family: Amaranth,Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  min-height: 100vh;
+  position: relative;
+  background-color: #f1f1f1;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <NavigationBar/>
+    <ProfieView/>
+    <AboutView/>
+  </div>
+</template>
+
+<script setup>
+import  NavigationBar  from "@/components/NavigationBar.vue";
+import  ProfieView  from "@/components/ProfileView.vue";
+import  AboutView  from "@/components/AboutView";
+
+
+</script>
+ 
+<style  scoped>
+
+    
+
+</style>
